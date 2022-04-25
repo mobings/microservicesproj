ec150e1d94fe595fe7956ac0417384b3807b1c72
M M <mobyrish@gmail.com>
2022-04-25 08:37:14 +0100

    bbbb

--- MainClient/src/test/java/com/ait/proj/test/MicroSvcClientTests.java
+++ MainClient/src/test/java/com/ait/proj/test/MicroSvcClientTests.java
@@ -1,15 +1,24 @@
 package com.ait.proj.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+
 public class MicroSvcClientTests {
 
 	@Test
-	public void contextLoads() {
-	}
+    public void testConcatenate() {
+  String mytest="test";
+
+        String result = mytest.concat("contcated");
+
+        assertNotEquals("testconcated", result);
+
+    }
 
 }
